acacc9cf9072d3627432556688e5fc89600ccca7
emnaouerghemmi <emna.ouerghemmi@esprit.tn>
2024-11-13 23:26:32 +0100

    update

--- src/main/java/com/esprit/tic/twin/springproject/controllers/ChambreRestController.java
+++ src/main/java/com/esprit/tic/twin/springproject/controllers/ChambreRestController.java
@@ -1,11 +1,44 @@
 package com.esprit.tic.twin.springproject.controllers;
 
+import com.esprit.tic.twin.springproject.entities.Chambre;
 import com.esprit.tic.twin.springproject.services.IChambreService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/chambres")
 public class ChambreRestController {
     IChambreService chambreService;
+
+    @GetMapping
+    public List<Chambre> getAllChambres() {
+        return chambreService.retrieveAllChambres();
+    }
+
+
+    @GetMapping("/{id}")
+    public Chambre getChambreById(@PathVariable Long id) {
+        return chambreService.retrieveChambre(id);
+    }
+
+
+    @PostMapping
+    public Chambre addChambre(@RequestBody Chambre chambre) {
+        return chambreService.addChambre(chambre);
+    }
+
+    @PutMapping("/{id}")
+    public Chambre updateChambre(@PathVariable Long id, @RequestBody Chambre chambre) {
+        chambre.setIdChambre(id);  // S'assurer que l'ID est correctement défini
+        return chambreService.updateChambre(chambre);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public void deleteChambre(@PathVariable Long id) {
+        chambreService.removeChambre(id);
+    }
 }
--- src/main/java/com/esprit/tic/twin/springproject/controllers/EtudiantRestController.java
+++ src/main/java/com/esprit/tic/twin/springproject/controllers/EtudiantRestController.java
@@ -1,11 +1,45 @@
 package com.esprit.tic.twin.springproject.controllers;
 
+import com.esprit.tic.twin.springproject.entities.Etudiant;
 import com.esprit.tic.twin.springproject.services.IEtudiantService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/etudiants")
 public class EtudiantRestController {
     IEtudiantService etudiantService;
+
+
+    @GetMapping
+    public List<Etudiant> getAllEtudiants() {
+        return etudiantService.retrieveAllEtudiants();
+    }
+
+
+    @GetMapping("/{id}")
+    public Etudiant getEtudiantById(@PathVariable Long id) {
+        return etudiantService.retrieveEtudiant(id);
+    }
+
+    @PostMapping
+    public Etudiant addEtudiant(@RequestBody Etudiant etudiant) {
+        return etudiantService.addEtudiant(etudiant);
+    }
+
+
+    @PutMapping("/{id}")
+    public Etudiant updateEtudiant(@PathVariable Long id, @RequestBody Etudiant etudiant) {
+        etudiant.setIdEtudiant(id);  // S'assurer que l'ID est correctement défini
+        return etudiantService.updateEtudiant(etudiant);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public void deleteEtudiant(@PathVariable Long id) {
+        etudiantService.removeEtudiant(id);
+    }
 }
