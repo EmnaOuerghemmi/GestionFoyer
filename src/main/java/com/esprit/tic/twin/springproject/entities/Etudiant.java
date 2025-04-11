e1ebd185e4e2685d22e41d96e5e743dd03e95b17
emnaouerghemmi <emna.ouerghemmi@esprit.tn>
2025-04-11 16:13:34 +0200

    first commit

--- src/main/java/com/esprit/tic/twin/springproject/entities/Etudiant.java
+++ src/main/java/com/esprit/tic/twin/springproject/entities/Etudiant.java
@@ -1,5 +1,6 @@
 package com.esprit.tic.twin.springproject.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
@@ -29,8 +30,11 @@ public class Etudiant implements Serializable {
     @Enumerated(EnumType.STRING)
     private TypeEtudiant typeEtudiant;
     @ManyToMany
+    @JsonIgnore
     private Set<Reservation> reservations;
+
     @OneToMany(mappedBy = "etudiantOrdinaire")
+    @JsonIgnore
     private Set<Tache> taches;
     @OneToOne
     private Tache tache;
--- src/main/java/com/esprit/tic/twin/springproject/services/EtudiantServiceImpl.java
+++ src/main/java/com/esprit/tic/twin/springproject/services/EtudiantServiceImpl.java
@@ -45,21 +45,22 @@ public class EtudiantServiceImpl implements IEtudiantService{
     }
     @Override
     public Etudiant affecterEtudiantAReservation(String nomEt, String prenomEt, String idReservation) {
-        //Same work every time we have many-to-many relation
         Etudiant e = etudiantRepository.findByNomEtAndPrenomEt(nomEt, prenomEt);
         Reservation r = reservationRepository.findByIdReservation(idReservation);
 
-        //Etudiant is the parent cause mappedBy is in reservation so reservation is child!
-        Set<Reservation> reservationsMisesAJour = new HashSet<Reservation>();
-        if(e.getReservations()!=null) {
-            reservationsMisesAJour = e.getReservations();
+        if (e.getReservations() != null) {
+            e.getReservations().size(); // Force le chargement des réservations existantes
         }
+
+        Set<Reservation> reservationsMisesAJour = e.getReservations();
         reservationsMisesAJour.add(r);
         e.setReservations(reservationsMisesAJour);
+
         etudiantRepository.save(e);
 
         return e;
     }
+
 
     @Override
     public HashMap<String, Float> calculNouveauMontantInscriptionDesEtudiants(String nomEt, String prenomEt) {
